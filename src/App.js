e627f197bd1f62cbd171be77e604cdcc17c9b2d7
Carolina Ochoa <carolina.ochoa.g@disruptia.co>
2023-01-26 16:30:58 -0500

    configuration commit and footer and nav components structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
+import Nav from './components/nav/nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+      <Nav />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nuestro MVP esta online!!.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="Disruptia"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Disruptia          
+        </a>              
+      </header>    
+      
     </div>
+    
   );
 }
 
---
+++ src/components/nav/nav.js
@@ -0,0 +1,10 @@
+
+function Nav() {
+  return (
+    <div className="App">
+      <h1>NAV COMPONENT</h1>
+    </div>
+  );
+}
+
+export default Nav;
